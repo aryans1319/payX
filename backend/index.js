3cf1062f35d901a953b9c1a445a0ffaf877d7d0c
Aryan <aryan4p130202@gmail.com>
2024-02-23 21:23:36 +0530

    feat: server routing structure setup

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const apiRoutes = require("./routes/index")
 const connectDB = require("./config/db");
-
 const app = express();
 
 connectDB();
 
 const PORT = 5000;
+
+app.use("/api", apiRoutes);
+
+app.get("/", (req, res) => {
+    res.send('Server chal rha guys!')
+})
 
 app.listen(PORT, console.log(`Server running on ${PORT}.`))
